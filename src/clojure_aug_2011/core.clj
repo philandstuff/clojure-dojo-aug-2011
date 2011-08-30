244c97c940cdc2687a8b19a5e12cd2e0542bbabe
Philip Potter <philipgpotter@gmail.com>
2011-08-30 21:25:03 +0100

    Autocommit

--- src/clojure_aug_2011/core.clj
+++ src/clojure_aug_2011/core.clj
@@ -8,21 +8,30 @@
 
 (defn indexed-board [board] (vector (seq board)))
 
-(defn instant-horiz-win [board]
+#_(defn row-is-win [ib c1 c2 c3]
+  (let [player (which-player ib)
+	contents (map ib [c1 c2 c3])]
+    (when
+	(and
+	 (= 2 (count #(= player %) contents)
+	      )
+	 (= 1 (count #(= \- %) contents))))))
+
+#_(defn instant-horiz-win [board]
   (let [player (which-player board)
 	ib (indexed-board board)]
-    
+        (row-is-win ib 0 1 2)
 	))
 
-(defn instant-win [board]
+#_(defn instant-win [board]
   (instant-horiz-win board))
 
 (defn choice [board]
   (cond ;(instant-win board) (instant-win board)
 	(= \- (nth board 4)) 4
 	(= \- (nth board 2)) 2
-	(= \- (nth board 0)) 0
 	(= \- (nth board 6)) 6
+	(= \- (nth board 0)) 0
 	(= \- (nth board 8)) 8
 	:else (.indexOf board "-")))
 
