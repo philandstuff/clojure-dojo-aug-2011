d83b9a6cf616089ea51c19122a7d3a9819e74cbf
Philip Potter <philipgpotter@gmail.com>
2011-08-30 20:52:55 +0100

    Autocommit

--- src/clojure_aug_2011/core.clj
+++ src/clojure_aug_2011/core.clj
@@ -1,13 +1,24 @@
 (ns clojure-aug-2011.core
   (:gen-class))
 
+(defn instant-horiz-win [board]
+  (let [cells (seq board)
+	row1 (take 3 cells)
+	row2 (take 3 (drop 3 cells))
+	row3 (take 3 (drop 6 cells))]
+    ))
+
+(defn instant-win [board]
+  (instant-horiz-win board))
+
 (defn choice [board]
-  (cond (= \- (nth board 4)) 5
-	(= \- (nth board 0)) 1
-	(= \- (nth board 6)) 7
-	(= \- (nth board 2)) 3
-	(= \- (nth board 8)) 9
-	:else (inc (.indexOf board "-"))))
+  (cond ;;(instant-win board) (instant-win board)
+	(= \- (nth board 4)) 4
+	(= \- (nth board 0)) 0
+	(= \- (nth board 6)) 6
+	(= \- (nth board 2)) 2
+	(= \- (nth board 8)) 8
+	:else (.indexOf board "-")))
 
 (defn -main [prev]
   (println (choice prev)))
