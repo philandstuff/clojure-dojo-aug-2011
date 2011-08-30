2db1fc16f103008934487c582e215162410a1609
Philip Potter <philipgpotter@gmail.com>
2011-08-30 20:43:23 +0100

    Autocommit

--- src/clojure_aug_2011/core.clj
+++ src/clojure_aug_2011/core.clj
@@ -2,7 +2,12 @@
   (:gen-class))
 
 (defn choice [board]
-  (inc (.indexOf board "-")))
+  (cond (= \- (nth board 4)) 5
+	(= \- (nth board 0)) 0
+	(= \- (nth board 6)) 6
+	(= \- (nth board 2)) 2
+	(= \- (nth board 8)) 8
+	:else (inc (.indexOf board "-"))))
 
 (defn -main [prev]
   (println (choice prev)))
