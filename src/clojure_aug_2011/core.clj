8e4e5241798d607b359fdbf19832a2157d5bc208
Philip Potter <philipgpotter@gmail.com>
2011-08-30 21:13:31 +0100

    Autocommit

--- src/clojure_aug_2011/core.clj
+++ src/clojure_aug_2011/core.clj
@@ -6,13 +6,16 @@
 	os (count (filter #(= \0 %) board))]
     (if (= xs os) \0 \x)))
 
+(defn indexed-board [board] (vector (seq board)))
+
 (defn instant-horiz-win [board]
   (let [player (which-player board)
 	cells (seq board)
 	row1 (take 3 cells)
 	row2 (take 3 (drop 3 cells))
 	row3 (take 3 (drop 6 cells))]
-    ))
+    
+	))
 
 (defn instant-win [board]
   (instant-horiz-win board))
@@ -20,9 +23,9 @@
 (defn choice [board]
   (cond ;(instant-win board) (instant-win board)
 	(= \- (nth board 4)) 4
+	(= \- (nth board 2)) 2
 	(= \- (nth board 0)) 0
 	(= \- (nth board 6)) 6
-	(= \- (nth board 2)) 2
 	(= \- (nth board 8)) 8
 	:else (.indexOf board "-")))
 
--- test/clojure_aug_2011/test/core.clj
+++ test/clojure_aug_2011/test/core.clj
@@ -5,3 +5,6 @@
 (deftest test-which-player
   (is (= \x (which-player "0--------")))
 	(is (= \0 (which-player "0-x------"))))
+	
+(deftest test-insta-win
+	(is (= 2 (instant-win "00-xx----"))))
